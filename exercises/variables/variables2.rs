37d4bd18e9765bfb2f858bcb1e54a819e4a5d060
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-02-02 18:57:03 -0800

    Progress

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,13 +1,16 @@
 // variables2.rs
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let x;
+    let x: u32;
+    x = 10;
+
     if x == 10 {
         println!("x is ten!");
     } else {
         println!("x is not ten!");
     }
 }
+
